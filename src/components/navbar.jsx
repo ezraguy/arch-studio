4414cf57746d949ebedc564a08afa2cbfac524cd
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-09-25 20:15:11 +0300

    ui changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../scss/navbar.scss';
 import logo from '../images/logo.svg'
+import hamburger from '../images/icons/icon-hamburger.svg';
+import closeIcon from '../images/icons/icon-close.svg';
 const Navbar = () => {
+    const [navItemsClass, setNavItemsClass] = useState(false);
+    const [navBarClass, setNavBarClass] = useState(false);
+    const [navIcon, setNavIcon] = useState(false);
+    const [overlay, setOverlay] = useState(false);
+    const handleMobileNav = () => {
+        setNavItemsClass(!navItemsClass);
+        setNavIcon(!navIcon);
+        setOverlay(!overlay);
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            console.log(window.pageYOffset);
+            if (window.pageYOffset > 100) {
+                setNavBarClass(true);
+            }
+            else {
+
+                setNavBarClass(false);
+            }
+        })
+    }, [])
+
+
     return (
-        <div className="navbar">
-            <img src={logo} alt="logo" className="logo" />
-            <div className="nav-items">
-                <div className="nav-item">
-                    <p>Portfolio</p>
+        <React.Fragment>
+            <div className={navBarClass ? "navbar fixed" : "navbar"}>
+                <img src={logo} alt="logo" className="logo" />
+                <div className={navItemsClass ? 'nav-items show' : 'nav-items'}>
+                    <div className="nav-item">
+                        <p>Portfolio</p>
+                    </div>
+                    <div className="nav-item">
+                        <p>About Us</p>
+                    </div>
+                    <div className="nav-item">
+                        <p>Contact</p>
+                    </div>
 
                 </div>
-                <div className="nav-item">
-                    <p>About Us</p>
-
-                </div>
-                <div className="nav-item">
-
-                    <p>Contact</p>
-                </div>
+                <button onClick={handleMobileNav} className="burger-icon">
+                    <img src={navIcon ? closeIcon : hamburger} alt="burger-icon" /></button>
             </div>
-        </div>
+            {overlay &&
+                <div className="overlay"></div>
+            }
+        </React.Fragment>
     );
 }
 
